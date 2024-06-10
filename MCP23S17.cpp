5810ada2ad2c069c8b0bfbd40243d76a14e3836e
olkal <olav.kallhovd@gmail.com>
2024-06-10 19:43:47 +0200

    Update MCP23S17.cpp

--- MCP23S17.cpp
+++ MCP23S17.cpp
@@ -66,7 +66,7 @@ bool MCP23S17::begin(bool pullup)
   if (! isConnected()) return false;
 
   //  disable address increment - datasheet P20
-  //  readReg16() and writeReg16() will not work with address increment disabled.
+  //  note that readReg16() and writeReg16() will not work with address increment disabled.
   //    SEQOP: Sequential Operation mode bit
   //    1 = Sequential operation disabled, address pointer does not increment.
   //    0 = Sequential operation enabled, address pointer increments.
