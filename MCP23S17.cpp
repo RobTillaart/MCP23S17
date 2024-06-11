10679fb621ae0a9563645f518820a96813ed745a
olkal <olav.kallhovd@gmail.com>
2024-06-11 15:23:24 +0200

    Update MCP23S17.cpp
    MCP23017_IOCR to MCP23x17_IOCR

--- MCP23S17.cpp
+++ MCP23S17.cpp
@@ -71,11 +71,11 @@ bool MCP23S17::begin(bool pullup)
   //    1 = Sequential operation disabled, address pointer does not increment.
   //    0 = Sequential operation enabled, address pointer increments.
   //  if (! writeReg(MCP23017_IOCR, MCP23017_IOCR_SEQOP)) return false;
-  uint8_t reg = readReg(MCP23017_IOCR);
-  if (reg & MCP23017_IOCR_SEQOP)  //  check if already zero
+  uint8_t reg = readReg(MCP23x17_IOCR);
+  if (reg & MCP23x17_IOCR_SEQOP)  //  check if already zero
   {
-    reg &= ~MCP23017_IOCR_SEQOP;  //  clear SEQOP bit for sequential read/write
-    if (! writeReg(MCP23017_IOCR, reg)) return false;
+    reg &= ~MCP23x17_IOCR_SEQOP;  //  clear SEQOP bit for sequential read/write
+    if (! writeReg(MCP23x17_IOCR, reg)) return false;
   }
 
   if (pullup)
